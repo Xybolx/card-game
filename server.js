e078959ba52399820f333323313e1fee30cb7205
Xybolx <xybolx@gmail.com>
2019-02-11 14:48:08 -0700

    "war game"

---
+++ server.js
@@ -0,0 +1,33 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+var card = require("card-deck");
+var mysql = require("mysql");
+
+
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var port = process.env.PORT || 3000;
+var port2 = process.env.PORT2 || 8888;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(port, function() {
+    console.log("App listening on PORT " + port);
+  });
+  
+//   app.listen(port2, function() {
+//     console.log("App listening on PORT " + port2);
+//   });    
